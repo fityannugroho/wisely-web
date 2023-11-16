26b049a838a877126122799c514e8b16a17276fa
Fityan <fityannugroho@gmail.com>
2023-11-16 15:43:46 +0700

    fix: `fetch` the built in charset & parse it as `options.charSets` args

--- src/pages/api/wisely.ts
+++ src/pages/api/wisely.ts
@@ -1,11 +1,20 @@
 import type { APIRoute } from 'astro';
-import wisely from 'wisely';
+import wisely, { type CharSet, type CharSetNames } from 'wisely';
 
 export const prerender = false;
+
+async function fetchBuiltInCharSet(charSet: CharSetNames): Promise<CharSet> {
+  const response = await fetch(`https://cdn.jsdelivr.net/npm/wisely@0.2.0/charsets/${charSet}.json`);
+  const json = await response.json();
+  return json as CharSet;
+}
 
 export const GET: APIRoute = async ({ request }) => {
   const { searchParams } = new URL(request.url);
   const text = searchParams.get('q');
+
+  // Load the default charSet
+  const charSet = await fetchBuiltInCharSet('latin');
 
   if (!text) {
     return new Response(JSON.stringify({
@@ -21,7 +30,7 @@ export const GET: APIRoute = async ({ request }) => {
   return new Response(JSON.stringify({
     status: 200,
     message: 'OK',
-    text: wisely({ text }),
+    text: wisely({ text, charSets: [charSet] }),
   }), {
     status: 200,
     headers: {'Content-Type': 'application/json; charset=utf-8'},
