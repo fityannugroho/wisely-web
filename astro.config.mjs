ff0bc0a8496d3196dcf26e5f8117eb0a49b09ce6
Fityan <fityannugroho@gmail.com>
2023-11-21 15:44:23 +0700

    add meta tags and og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,16 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from 'astro/config';
 
+// https://vercel.com/docs/projects/environment-variables/system-environment-variables#system-environment-variables
+const VERCEL_PREVIEW_SITE = process.env.VERCEL_ENV !== 'production'
+  && process.env.VERCEL_URL
+  && `https://${process.env.VERCEL_URL}`;
+
+const site = VERCEL_PREVIEW_SITE || 'https://wisely.vercel.app/';
+
 // https://astro.build/config
 export default defineConfig({
+  site,
   integrations: [
     starlight({
       title: 'Wisely',
@@ -41,6 +49,36 @@ export default defineConfig({
         baseUrl: 'https://github.com/fityannugroho/wisely-web/edit/main/',
       },
       lastUpdated: true,
+      head: [
+        {
+          tag: 'meta',
+          attrs: {
+            name: 'keywords',
+            content: 'wisely, npm, package, github, tools, obfuscator, obfuscate, text, phrase, random, uncommon, character, web, playground',
+          },
+        },
+        {
+          tag: 'meta',
+          attrs: {
+            property: 'og:image',
+            content: site + 'og.jpg',
+          },
+        },
+        {
+          tag: 'meta',
+          attrs: {
+            property: 'twitter:card',
+            content: 'summary_large_image',
+          },
+        },
+        {
+          tag: 'meta',
+          attrs: {
+            property: 'twitter:image',
+            content: site + 'og.jpg',
+          },
+        },
+      ],
     }),
     react(),
     tailwind({
